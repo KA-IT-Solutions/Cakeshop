f8761d1fc78a19af6d4360838a5accfb92d91ca6
PreetiKardure <preetikardure819@gmail.com>
2024-07-20 13:52:51 +0530

    created hero page

--- cake-shop/src/Components/Home/Home.js
+++ cake-shop/src/Components/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div className="hero">
+      <div className="hero-content">
+        <h1>Welcome to Our Website</h1>
+        <p>Your journey begins here</p>
+        <button className="hero-button">Get Started</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home
