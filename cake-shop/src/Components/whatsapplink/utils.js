1b87674bd0dc48191fd9bb34efc65ddc33740482
Rutuja Ruke <rutuja.ruke2004@gmail.com>
2024-07-29 15:34:28 +0530

    ok

--- cake-shop/src/Components/whatsapplink/utils.js
+++ cake-shop/src/Components/whatsapplink/utils.js
@@ -3,7 +3,7 @@
 
 // src/utils.js
 export const generateWhatsAppLink = (cake) => {
-    const phoneNumber = '1234567890'; // Your phone number
+    const phoneNumber = '9764065434'; // Your phone number
     const message = `I would like to order a ${cake.name} (${cake.category}) for ${cake.price}.`;
     return `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 };
